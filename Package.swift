bb55574d6bc30f76aca1948e6699c94d2ef382d5
akardas16 <akardas16@ku.edu.tr>
2022-08-21 09:32:47 +0300

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,11 @@ let package = Package(
         // Targets can depend on other targets in this package, and on products in packages this package depends on.
         .target(
             name: "SideMenu",
-            dependencies: [],path: "Sources"),
+            dependencies: [],
+            path: "Sources", resources: [
+                .process("Resources/Media.xcassets")
+            ]),
+    
        
     ]
 )
